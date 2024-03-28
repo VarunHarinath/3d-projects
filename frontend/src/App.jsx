e4b40d2d99e0a3cb53170164e4528405eaeacff4
Varun Harinath <varunharinath15@gmail.com>
2024-03-29 04:48:37 +0530

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,16 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 // Componenets Importing
 import Lessons from "./pages/Lessons.jsx";
 import "./App.css";
-// import ModuleQuiz from "./components/ModuleQuiz.jsx";
+import ModuleQuiz from "../src/components/ModuleQuiz.jsx";
 import QuizzList from "./pages/QuizzList.jsx";
 import Navbar from "./Components/Navbar";
-// import Leaderboard from "./components/Leaderboard.jsx";
+import Leaderboard from "../src/components/Leaderboard.jsx";
 import { Home, About, Projects, Contact } from "../src/pages/index.js";
 import DSAPage from "./pages/lessonPages/DSAPage.jsx";
 import AlgoPage from "./pages/lessonPages/AlgoPage.jsx";
 import IndetailPage from "./pages/lessonPages/IndetailPageAlgo.jsx";
 import IndetailPageDsa from "./pages/lessonPages/IndetailPageDsa.jsx";
-import SocketIo from "./components/SocketIoC/SocketIo.jsx";
+import SocketIo from "../src/components/SocketIoC/SocketIo.jsx";
 function App() {
   return (
     <>
@@ -22,7 +22,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/quizz" element={<QuizzList />} />
-          {/* <Route path="/quizz/1" element={<ModuleQuiz />} /> */}
+          <Route path="/quizz/1" element={<ModuleQuiz />} />
           <Route path="/lessons" element={<Lessons />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/projects" element={<Projects />} />
