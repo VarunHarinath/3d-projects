bcf917bd1df87088bf45041a5688088e41239bc3
Varun Harinath <varunharinath15@gmail.com>
2024-03-29 04:33:18 +0530

    update on server ip

--- frontend/src/pages/HomeInfo.jsx
+++ frontend/src/pages/HomeInfo.jsx
@@ -7,7 +7,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
         Welcome to
-        <span className="font-semibold mx-2 text-white">??APP TITLE ??</span>
+        <span className="font-semibold mx-2 text-white">Smart Edu</span>
         👋
         <br />
         Your ultimate Learning Companion
