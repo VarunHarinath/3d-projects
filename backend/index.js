68de29cb7901595eb2f897fd5ef2b37da4891a83
Varun Harinath <varunharinath15@gmail.com>
2024-03-29 06:21:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,7 @@ import cors from "cors";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
-app.use(
-  cors({
-    origin: "https://smartedu-adqua3res-varunharinath.vercel.app", // Replace with your Vercel domain
-  })
-);
+
 
 app.use("/lessonDsa", lessonsRoute);
 app.use("/lessonAlgo", lessonAlgoRoute);
